0081bbb64947ba828a9a2d1a7f848823cd00a1dd
Fernando Corrêa da Silva <fernando_silvasc@hotmail.com>
2020-10-22 20:34:41 -0700

    finished mobile version 1.0.0

--- mobile/src/pages/CreateOrphanage/SelectMapPosition.tsx
+++ mobile/src/pages/CreateOrphanage/SelectMapPosition.tsx
@@ -1,89 +1,89 @@
-import React, { useEffect, useState } from 'react';
-import { View, StyleSheet, Dimensions, Text } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { View, StyleSheet, Dimensions, Text } from "react-native";
 
-import { useNavigation } from '@react-navigation/native';
-import { RectButton } from 'react-native-gesture-handler';
-import MapView, { Marker, MapEvent } from 'react-native-maps';
+import { useNavigation } from "@react-navigation/native";
+import { RectButton } from "react-native-gesture-handler";
+import MapView, { Marker, MapEvent } from "react-native-maps";
 import usePosition from "../../hooks/usePosition";
 
-import mapMarkerImg from '../../images/map-marker.png';
-
-
-
+import mapMarkerImg from "../../images/map-marker.png";
 
 export default function SelectMapPosition() {
-  
   const navigation = useNavigation();
   const getPosition = usePosition();
   const { latitude, longitude } = getPosition.currentPosition;
 
-  const [position, setPosition] = useState({ latitude: 0, longitude: 0})
- 
-  function handleNextStep() {
-    navigation.navigate('OrphanageData');
-  };
+  const [position, setPosition] = useState({ latitude: 0, longitude: 0 });
 
-  function handleSelectMapPosition( event: MapEvent) {
+  function handleNextStep() {
+    navigation.navigate("OrphanageData", { position });
+  }
+
+  function handleSelectMapPosition(event: MapEvent) {
     setPosition(event.nativeEvent.coordinate);
   }
 
-  console.log(latitude, longitude)
-
+  console.log(latitude, longitude);
 
   return (
     <View style={styles.container}>
-      <MapView 
+      <MapView
         initialRegion={{
-          latitude: latitude,
-          longitude: longitude,
-          latitudeDelta: 0.008,
-          longitudeDelta: 0.008,
+          latitude: 49.1749376,
+          longitude: -122.8242944,
+          latitudeDelta: 0.05,
+          longitudeDelta: 0.05,
         }}
         style={styles.mapStyle}
-        onPress={handleSelectMapPosition}
-      >
-        { position.latitude !== 0 && (
-        <Marker 
-          icon={mapMarkerImg}
-          coordinate={{ latitude: position.latitude, longitude: position.longitude }}
-        />
-        ) }
+        onPress={handleSelectMapPosition}>
+        {position.latitude !== 0 && (
+          <Marker
+            icon={mapMarkerImg}
+            coordinate={{
+              latitude: position.latitude,
+              longitude: position.longitude,
+            }}
+          />
+        )}
       </MapView>
 
+      {position.latitude !== 0 && (
       <RectButton style={styles.nextButton} onPress={handleNextStep}>
         <Text style={styles.nextButtonText}>Next</Text>
       </RectButton>
+      )}
+
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    position: 'relative'
+    position: "relative",
   },
 
   mapStyle: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
   },
 
   nextButton: {
-    backgroundColor: '#15c3d6',
+    backgroundColor: "#15c3d6",
     borderRadius: 20,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     height: 56,
 
-    position: 'absolute',
+    position: "absolute",
     left: 24,
     right: 24,
     bottom: 40,
   },
 
   nextButtonText: {
-    fontFamily: 'Nunito_800ExtraBold',
+    fontFamily: "Nunito_800ExtraBold",
     fontSize: 16,
-    color: '#FFF',
-  }
-})
+    color: "#FFF",
+  },
+});
--- mobile/src/pages/OrphanagesMap.tsx
+++ mobile/src/pages/OrphanagesMap.tsx
@@ -1,10 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View, Dimensions } from "react-native";
 import { RectButton } from "react-native-gesture-handler";
 import MapView, { Marker, Callout, PROVIDER_GOOGLE } from "react-native-maps";
 import { Feather } from "@expo/vector-icons";
 import mapMarker from "../images/map-marker.png";
-import { useNavigation } from "@react-navigation/native";
+import { useNavigation, useFocusEffect } from "@react-navigation/native";
 import api from "../services/api";
 
 interface Orphanage {
@@ -18,11 +18,11 @@ function OrphanagesMap() {
   const [orphanages, setOrphanages] = useState<Orphanage[]>([]);
   const navigation = useNavigation();
 
-  useEffect(() => {
+  useFocusEffect(() => {
     api.get("orphanages").then((res) => {
       setOrphanages(res.data);
     });
-  }, []);
+  });
 
 
   function handleNavigationOrphanageDetails(id: number) {
